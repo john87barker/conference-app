70e50f1432f716b7df6e6ab6fe3b016c57c3ed94
john87barker <john87barker@gmail.com>
2021-11-17 09:20:23 -0700

    sessiondetails set up

---
+++ src/client/modules/my/sessionDetails/sessionDetails.js
@@ -0,0 +1,14 @@
+import { LightningElement, api } from 'lwc';
+import { getSession } from '../../data/sessionService/sessionService';
+
+export default class SessionDetails extends LightningElement{
+  session;
+  @api
+  set sessionId(id) {
+    this._sessionId = id;
+    this.session = getSession(id);
+  }
+  get sessionId() {
+    return this._sessionId;
+  }
+}
